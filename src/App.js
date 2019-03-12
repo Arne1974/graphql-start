a408933d97d8fe70e04d9f464a33b884ef3ad8cc
Arne Siemens <arsiemens@gmail.com>
2019-03-12 13:12:01 +0100

    Export Recipes-component and exclude query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import ApolloClient from 'apollo-boost';
-import gql from 'graphql-tag';
-import { ApolloConsumer, ApolloProvider, Query } from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
+import Recipes from './Recipes';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/'
@@ -13,27 +13,7 @@ class App extends Component {
     return (
       <ApolloProvider client={client}>
         <h1>Hello World!</h1>
-        <Query 
-          query={gql`
-                  {
-                    recipes {
-                      id
-                      title
-                    }
-                  }
-                `}>
-            {({data}) => {
-              if (data.recipes === undefined) return null;
-
-              return (
-                <ul>
-                  {data.recipes.map(
-                    ({id, title}) => <li key={id}>{title}</li>
-                  )}
-                </ul>
-              )
-            }}
-          </Query>
+        <Recipes />
       </ApolloProvider>
     );
   }
